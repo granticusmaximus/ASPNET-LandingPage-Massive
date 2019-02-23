30166093f3b4985f744b0668ba8965e5199b6195
Grant Watson <grant@triumphdesigns.org>
2019-02-23 13:12:35 -0500

    Initial Commit

---
+++ coderush22/Controllers/MassiveController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coderush22.Controllers
+{
+    public class MassiveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult Index3()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
